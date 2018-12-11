da86cd67917d6332585baa787583d8301406e560
liaoguangjie <15600672381@163.com>
2018-12-11 22:47:06 +0800

    初始化

---
+++ redis-prcatice/src/main/java/com/redis/practice/RedisPrcaticeApplication.java
@@ -0,0 +1,12 @@
+package com.redis.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPrcaticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPrcaticeApplication.class, args);
+    }
+}
